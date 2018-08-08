b67bf5acb4a21c6ab6586da976fb4f870ee1a2da
ayushggarg <ayushggarg@gmail.com>
2018-08-08 18:57:16 +0530

    script modified

--- interest_quarterly_cummulative.py
+++ interest_quarterly_cummulative.py
@@ -12,20 +12,24 @@ time = int(input("Time (3/6/9/12): "))
 months = int(input("Months: "))
 print ("")
 
-counter = time
 amount = principle
 interest_total = 0
 tax_total = 0
 
-while (counter <= months):
-    interest = float(rate * amount * time / 1200)
-    interest = roundoff(interest)
-    interest_total += interest
-    tax = float(0.1 * interest)
-    tax = roundoff(tax)
-    tax_total += tax
-    amount = (interest - tax + amount) 
-    counter += time 
-    print (interest, tax, interest-tax, amount)
+for counter in range (time,months,time):
+	if (counter % 12 == 0):
+		print (interest_total, tax_total, interest_total-tax_total)
+		print ("")
+		interest_total = 0
+		tax_total = 0
+	
+	interest = float(rate * amount * time / 1200)
+	interest = roundoff(interest)
+	interest_total += interest
+	tax = float(0.1 * interest)
+	tax = roundoff(tax)
+	tax_total += tax
+	amount = (interest - tax + amount) 
+	print (interest, tax, interest-tax, amount)
 print (interest_total, tax_total, interest_total-tax_total)
 print ("")
